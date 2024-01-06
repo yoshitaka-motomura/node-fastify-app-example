6ad902d49bfa65b8d41bd0f153b8de11983d2c04
Yoshitaka motomura <stay.daybreak@gmail.com>
2024-01-07 02:42:06 +0900

    Delete unnecessary files and code

--- dist/controllers/pingController.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = async (request, reply) => {
-    //reply.header('Content-Type', 'application/json').code(200)
-    return JSON.stringify({ ping: 'pong' });
-};
--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const bootstrap_1 = require("./bootstrap");
-(async () => {
-    await (0, bootstrap_1.app)();
-})();
